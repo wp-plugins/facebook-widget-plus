6accfa448b1733a17b4b61afbd4726531381b652
riduan765 <riduan765@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-16 03:00:41 +0000

    stable released version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/facebook-widget-plus/trunk@949281 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ facebookWidgetPlus.php
@@ -0,0 +1,262 @@
+<?php
+/**
+ * @package facebook-widget-plus
+*/
+/*
+Plugin Name: Facebook Widget Plus
+Plugin URI: http://www.sdghouston.com
+Description: Thanks for installing Facebook Widget Plus
+Version: 1.0
+Author: Muhammad Riduan
+Author URI: http://www.sdghouston.com
+*/
+
+class FacebookWidgetPlus extends WP_Widget{
+    
+    public function __construct() {
+        $params = array(
+            'description' => 'Thanks for installing Facebook Widget',
+            'name' => 'Facebook Widget Plus'
+        );
+        parent::__construct('FacebookWidgetPlus','',$params);
+    }
+    
+    public function form($instance) {
+        extract($instance);
+        
+        ?>
+<!-- Color Picker Script Start -->
+<script type="text/javascript">
+//<![CDATA[
+    jQuery(document).ready(function()
+    {
+	// colorpicker field
+	jQuery('.cw-color-picker').each(function(){
+	var $this = jQuery(this),
+        id = $this.attr('rel');
+ 	$this.farbtastic('#' + id);
+    });
+});		
+//]]>   
+</script>
+<!-- Color Picker Script End -->
+<!-- here will put all widget configuration -->
+<p>
+    <label for="<?php echo $this->get_field_id('title');?>">Title : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('title');?>"
+	name="<?php echo $this->get_field_name('title');?>"
+        value="<?php echo !empty($title) ? $title : "Facebook Widget Plus"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('fb_url');?>">Facebook Page URL : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('fb_url');?>"
+	name="<?php echo $this->get_field_name('fb_url');?>"
+        value="<?php echo !empty($fb_url) ? $fb_url : "https://www.facebook.com/FacebookDevelopers"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('width');?>">Width : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('width');?>"
+	name="<?php echo $this->get_field_name('width');?>"
+        value="<?php echo !empty($width) ? $width : "300"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('height');?>">Height : </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('height');?>"
+	name="<?php echo $this->get_field_name('height');?>"
+        value="<?php echo !empty($height) ? $height : "600"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'color_scheme' ); ?>">Color Scheme:</label> 
+    <select id="<?php echo $this->get_field_id( 'color_scheme' ); ?>"
+        name="<?php echo $this->get_field_name( 'color_scheme' ); ?>"
+        class="widefat" style="width:100%;">
+            <option value="0" <?php if ($color_scheme == '0') echo 'selected="0"'; ?> >Light</option>
+            <option value="1" <?php if ($color_scheme == '1') echo 'selected="1"'; ?> >Dark</option>	
+    </select>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'face' ); ?>">Show Faces:</label> 
+    <select id="<?php echo $this->get_field_id( 'face' ); ?>"
+        name="<?php echo $this->get_field_name( 'face' ); ?>"
+        class="widefat" style="width:100%;">
+            <option value="true" <?php if ($face == 'true') echo 'selected="true"'; ?> >Yes</option>
+            <option value="false" <?php if ($face == 'false') echo 'selected="false"'; ?> >No</option>	
+    </select>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'header' ); ?>">Show Header:</label> 
+    <select id="<?php echo $this->get_field_id( 'header' ); ?>"
+        name="<?php echo $this->get_field_name( 'header' ); ?>"
+        class="widefat" style="width:100%;">
+            <option value="true" <?php if ($face == 'header') echo 'selected="header"'; ?> >Yes</option>
+            <option value="false" <?php if ($face == 'header') echo 'selected="header"'; ?> >No</option>	
+    </select>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'border' ); ?>">Show Border:</label> 
+    <select id="<?php echo $this->get_field_id( 'border' ); ?>"
+        name="<?php echo $this->get_field_name( 'border' ); ?>"
+        class="widefat" style="width:100%;">
+            <option value="true" <?php if ($border == 'true') echo 'selected="true"'; ?> >Yes</option>
+            <option value="false" <?php if ($border == 'false') echo 'selected="false"'; ?> >No</option>	
+    </select>
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id( 'post' ); ?>">Show Post:</label> 
+    <select id="<?php echo $this->get_field_id( 'post' ); ?>"
+        name="<?php echo $this->get_field_name( 'post' ); ?>"
+        class="widefat" style="width:100%;">
+            <option value="true" <?php if ($post == 'true') echo 'selected="true"'; ?> >Yes</option>
+            <option value="false" <?php if ($post == 'false') echo 'selected="false"'; ?> >No</option>	
+    </select>
+</p>
+<div style="color:#fff; font-size:12px; font-weight:bold; padding:3px; margin:0; text-align:center; background:#33302e;">CSS Customization</div>
+<p>
+    <label for="<?php echo $this->get_field_id('backgroundColor');?>">Background Color: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('backgroundColor');?>"
+	name="<?php echo $this->get_field_name('backgroundColor');?>"
+        value="<?php echo !empty($backgroundColor) ? $backgroundColor : "#1e9f75"; ?>" />
+</p>
+<div class="cw-color-picker backgroundColorHide" rel="<?php echo $this->get_field_id('backgroundColor'); ?>"></div>
+<p>
+    <label for="<?php echo $this->get_field_id('margin_css');?>">Margin: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('margin_css');?>"
+	name="<?php echo $this->get_field_name('margin_css');?>"
+        value="<?php echo !empty($margin_css) ? $margin_css : "10px 0px"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('padding_css');?>">Padding: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('padding_css');?>"
+	name="<?php echo $this->get_field_name('padding_css');?>"
+        value="<?php echo !empty($padding_css) ? $padding_css : "0px"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('border_css');?>">Border CSS: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('border_css');?>"
+	name="<?php echo $this->get_field_name('border_css');?>"
+        value="<?php echo !empty($border_css) ? $border_css : "1px solid #c7c7c7"; ?>" />
+</p>
+<p>
+    <label for="<?php echo $this->get_field_id('box_shadow');?>">Box Shadow: </label>
+    <input
+	class="widefat"
+	id="<?php echo $this->get_field_id('box_shadow');?>"
+	name="<?php echo $this->get_field_name('box_shadow');?>"
+        value="<?php echo !empty($box_shadow) ? $box_shadow : "0px 0px 14px #000000"; ?>" />
+</p>
+<?php
+    }
+    
+    public function widget($args, $instance) {
+        extract($args);
+        extract($instance);
+        $title = apply_filters('widget_title', $title);
+        $description = apply_filters('widget_description', $description);
+	if(empty($title)) $title = "Facebook Widget Plus";
+        if(empty($fb_url)) $fb_url = "http://www.facebook.com/FacebookDevelopers";
+        if(empty($width)) $width = "300";
+        if(empty($height)) $height = "600";
+        if(empty($color_scheme)) $color_scheme = "0";
+        if(empty($face)) $face = "true";
+        if(empty($header)) $header = "true";
+        if(empty($border)) $border = "true";
+        if(empty($post)) $post = "true";
+        if(empty($backgroundColor)) $backgroundColor = "#1e9f75";
+        if(empty($margin_css)) $margin_css = "10px 0px";
+        if(empty($padding_css)) $padding_css = "0px";
+        if(empty($border_css)) $padding_css = "1px solid #c7c7c7";
+        if(empty($box_shadow)) $box_shadow = "0px 0px 14px #000000";
+        
+        echo $before_widget;
+            echo $before_title . $title . $after_title;
+            
+            ?>
+
+<style>
+    
+    .fb-like-box {
+				background:<?php echo $backgroundColor; ?>;
+				padding:<?php echo $padding_css; ?>;
+				margin:<?php echo $margin_css; ?>;
+				border:<?php echo $border_css; ?>;
+				-webkit-box-shadow: <?php echo $box_shadow; ?>;
+				-moz-box-shadow: <?php echo $box_shadow; ?>;
+				box-shadow: <?php echo $box_shadow; ?>;
+               }
+</style>
+  <div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+        
+         <div class="facebook_widget_plus">
+
+	     <div class="fb-like-box" data-href="<?php echo $fb_url;?>"
+         
+           data-width="<?php echo $width;?>" 
+           data-height="<?php echo $height;?>"
+           
+          <?php if($color_scheme == '0') {?>
+                data-colorscheme="light"
+             <?php } else {?> 
+                data-colorscheme="dark"
+             <?php };?>
+             <?php if($face == 'true') {?>
+               data-show-faces="true"
+             <?php } else {?> 
+               data-show-faces="false"
+             <?php };?>
+             <?php if($header =='true') {?>
+               data-header="true" 
+             <?php } else {?> 
+               data-header="false" 
+             <?php };?>  <?php if($border=='true') {?>
+               data-show-border="true"
+             <?php } else {?> 
+               data-show-border="false"
+             <?php };?>
+             <?php if($post == 'true') {?>
+               data-stream="true" 
+             <?php } else {?> 
+               data-stream="false" 
+             <?php };?>
+        </div>
+	
+    </div>
+<?php
+        echo $after_widget;
+    }
+}
+//registering the color picker
+function facebook_widget_plus_color_picker_script() {
+	wp_enqueue_script('farbtastic');
+}
+function facebook_widget_plus_color_picker_style() {
+	wp_enqueue_style('farbtastic');
+}
+add_action('admin_print_scripts-widgets.php', 'facebook_widget_plus_color_picker_script');
+add_action('admin_print_styles-widgets.php', 'facebook_widget_plus_color_picker_style');
+add_action('widgets_init','register_FacebookWidgetPlus');
+function register_FacebookWidgetPlus(){
+    register_widget('FacebookWidgetPlus');
+}
---
+++ readme.txt
@@ -0,0 +1,90 @@
+=== Atom Twitter Feeds ===
+
+Plugin URI: http://www.sdghouston.com
+Author Name : Muhammad Riduan
+Author URL : http://www.sdghouston.com
+Tags: custom facebook likebox, facebook likebox, wordpress widget, wordpress facebook, advanced facebook
+Requires at least: 3.0.1
+Tested up to: 3.9.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Facebook - In this era a most popular work which is not only a social media site for connectivity with people as well become a business destination of your goods and products.
+
+== Description ==
+
+Facebook - In this era a most popular work which is not only a social media site for connectivity with people as well become a business destination of your goods and products.
+
+People won't re-visit on sites easily but though facebook you can advertise and can get much more business than a website can get for you.
+
+We're giving a free Wordpress Widget for these people who want to get more business from facebook.
+
+Features:
+
+1) its Free
+
+2) Tested on Wordpress Latest Versions
+
+3) Bug tested
+
+4) Light
+
+5) Shadow and Background color can be changed
+
+6) Lots of Custom Options available on backend.
+
+We really do appreciate any kind of feedback or suggestions so keep us inform what you like to have.
+
+== Installation ==
+
+First Install our plugin. Make sure you have activated it.
+
+As FACEBOOK WIDGET PLUS is a wordpress widget you have to find it on Widget Area. Go to Wordpress Widget for and drag and drop Facebook Widget Plus to prefer area of Wordpress Widget Positions.
+
+Facebook Widget Plus configurations are very basic. Their have some configuration fields described below::
+
+1. Title: Title of your wordpress widget. You can keep it blank as well.
+
+2. Facebook Page URL: The absolute URL of the Facebook Page that will be liked. This is a required setting.
+
+3. Width: Width of your widget in px.
+
+4. Height: Height of your widget area in px.
+
+5. Color Scheme: The color scheme used by the plugin. Can be "light" or "dark".
+
+6. Show Faces: Specifies whether to display profile photos of people who like the page.
+
+7. Show Header: Specifies whether to display the Facebook header at the top of the plugin.
+
+8. Show Border: Specifies whether or not to show a border around the plugin.
+
+9. Show Post: Specifies whether to display a stream of the latest posts by the Page.
+
+CSS Customization:
+
+1. Background Color: You can pick color from as you require or wish.
+
+2. Margin: Margin of widget in px.
+
+3. Padding: Padding of widget in px.
+
+4. Border CSS: CSS piece.
+
+5. Box Shadow: CSS piece.
+
+== Frequently Asked Questions ==
+You can directly contact us for any kind of support. - riduan@sdghouston.com
+
+== Screenshots ==
+
+1. Widget Frontend Screenshot (Like Button Only).
+2. Widget Frontend Screenshot (Likebox Only).
+3. Widget Frontend Screenshot (Likebox + Streams).
+4. Widget Backend Screenshot
+
+
+== Changelog ==
+
+= 1.0 =
+Stable version release
